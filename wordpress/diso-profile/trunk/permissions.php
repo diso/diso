815439f8c65eb0884e579d46ce44b1cc16fe5ea2
Stephen Paul Weber <singpolyma@gmail.com>
2008-02-03 16:33:13 +0000

    Major better UI

--- wordpress/diso-profile/trunk/permissions.php
+++ wordpress/diso-profile/trunk/permissions.php
@@ -57,34 +57,7 @@ function diso_permissions_page() {
 	global $userdata;
 
 	get_currentuserinfo();
-	if(count($_POST['permissions'])) {//if saving
-		$permissions = $userdata->profile_permissions;
-		foreach($_POST['permissions'] as $key => $val)
-			$permissions[$key] = array_keys($val);
-		update_usermeta($userdata->ID, 'profile_permissions', $permissions);
-		$userdata->profile_permissions = $permissions;
-	}//end if saving
 
-	$fields = array(
-		'First Name' => 'given-name',
-		'Middle Name(s)' => 'additional-name',
-		'Last Name' => 'family-name',
-		'Nickname' => 'nickname',
-		'E-mail Address' => 'email',
-		'Website(s)' => 'urls',
-		'AIM' => 'aim',
-		'Y!IM' => 'yim',
-		'Jabber' => 'jabber',
-		'About Me' => 'note',
-		'Photo' => 'photo',
-		'Organization' => 'org',
-		'Street Address' => 'street-address',
-		'City' => 'locality',
-		'Province/State' => 'region',
-		'Postal Code' => 'postal-code',
-		'Country' => 'country-name',
-		'Telephone Number' => 'tel',
-	);
 	$taxonomies = array(
 		'Yourself' => 'me',
 		'Friends' => 'friend',
@@ -108,22 +81,84 @@ function diso_permissions_page() {
 	$terms = get_terms('link_category');
 	foreach($terms as $term)
 		$taxonomies[$term->name] = $term->name;
+	if(count($_POST['permissions_level'])) {//if saving
+		//$permissions = $userdata->profile_permissions;
+		$permissions = array();
+		foreach($_POST['permissions_level'] as $field => $level) {
+			if($level == 'custom') {
+				foreach($_POST['permissions'][$field] as $key => $val)
+					$permissions[$key] = array_keys($val);
+			} else {
+				switch($level) {
+					case 'any':
+						$permissions[$field] = array_values($taxonomies);
+						break;
+					case 'family':
+						$permissions[$field] = array('kin', 'child', 'parent', 'sibling', 'spouse');
+						break;
+					case 'family,friends':
+						$permissions[$field] = array('kin', 'child', 'parent', 'sibling', 'spouse', 'friend', 'muse', 'date', 'sweetheart');
+						break;
+					case 'public':
+						$permissions[$field] = array();
+						break;
+				}//end switch
+			}//end if permissions_level
+		}//end foreach
+		update_usermeta($userdata->ID, 'profile_permissions', $permissions);
+		update_usermeta($userdata->ID, 'profile_permissions_level', $_POST['permissions_level']);
+		$userdata->profile_permissions = $permissions;
+		$userdata->profile_permissions_level = $_POST['permissions_level'];
+	}//end if saving
+
+	$fields = array(
+		'First Name' => 'given-name',
+		'Middle Name(s)' => 'additional-name',
+		'Last Name' => 'family-name',
+		'Nickname' => 'nickname',
+		'E-mail Address' => 'email',
+		'Website(s)' => 'urls',
+		'AIM' => 'aim',
+		'Y!IM' => 'yim',
+		'Jabber' => 'jabber',
+		'About Me' => 'note',
+		'Photo' => 'photo',
+		'Organization' => 'org',
+		'Street Address' => 'street-address',
+		'City' => 'locality',
+		'Province/State' => 'region',
+		'Postal Code' => 'postal-code',
+		'Country' => 'country-name',
+		'Telephone Number' => 'tel',
+	);
+
 	echo '<div class="wrap">';
 	echo '<h2>Change Profile Permissions</h2>';
-	echo '<b>Field &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; People who can view (none for public)</b>';
+	echo '<b>Field &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; People who can view</b>';
 	echo '<form method="post" action="">';
 	foreach($fields as $label => $field) {
 		echo '<div style="margin-bottom:1em;clear:both;">';
 		echo ' <label style="float:left;width:10em;" for="'.$field.'">'.$label.':</label> ';
+		echo '	<select id="'.$field.'-basic" name="permissions_level['.$field.']" onchange="if(this.value == \'custom\') { document.getElementById(\''.$field.'-custom\').style.display = \'block\';  } else { document.getElementById(\''.$field.'-custom\').style.display = \'none\'; }">';
+		echo '		<option '.($userdata->profile_permissions_level[$field] == 'public' ? 'selected="selected" ' : '').'value="public">Public</option>';
+		echo '		<option '.($userdata->profile_permissions_level[$field] == 'any' ? 'selected="selected" ' : '').'value="any">Any Contact</option>';
+		echo '		<option '.($userdata->profile_permissions_level[$field] == 'family' ? 'selected="selected" ' : '').'value="family">Family</option>';
+		echo '		<option '.($userdata->profile_permissions_level[$field] == 'family,friends' ? 'selected="selected" ' : '').'value="family,friends">Family and Friends</option>';
+		echo '		<option '.($userdata->profile_permissions_level[$field] == 'custom' ? 'selected="selected" ' : '').'value="custom">Custom</option>';
+		echo '	</select>';
+		echo '	<div id="'.$field.'-custom" '.($userdata->profile_permissions_level[$field] != 'custom' ? 'style="display:none;" ' : '' ).'>';
 		$c = 0;
+		echo '<div style="float:left;clear:left;width:10em;">&nbsp;</div>';
 		foreach($taxonomies as $ilabel => $term) {
-			echo '<div style="float:left;width:11em;"><input type="checkbox" name="permissions['.htmlentities($field).']['.htmlentities($term).']"'.(@in_array($term, $userdata->profile_permissions[$field]) ? ' checked="checked"' : '').' />&nbsp;'.$ilabel.'</div>';
+			echo '<div style="float:left;width:11em;">';
+			echo '<input type="checkbox" name="permissions['.htmlentities($field).']['.htmlentities($term).']"'.(@in_array($term, $userdata->profile_permissions[$field]) ? ' checked="checked"' : '').' />&nbsp;'.$ilabel.'</div>';
 			$c++;
 			if($c > 4) {
 				$c = 0;
 				echo '<div style="float:left;clear:left;width:10em;">&nbsp;</div>';
 			}
 		}//end
+		echo '	</div>';
 		echo '</div>';
 	}//end foreach fields
 	echo ' <input style="clear:both;margin-top:1em;" type="submit" value="Save &raquo;" /> ';
