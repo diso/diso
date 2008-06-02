6e42746acb47722f82885ef39676b9bebdaed02c
steveivy <steveivy@1fe52bb4-7940-0410-8bb1-4d0b51536974>
2008-06-02 19:21:20 +0000

    pheh. too many small tweaks to list. bad coder, bad.

--- movabletype/Friends/trunk/Friends/lib/Friends/URI.pm
+++ movabletype/Friends/trunk/Friends/lib/Friends/URI.pm
@@ -9,18 +9,13 @@ __PACKAGE__->install_properties(
         datasource => 'diso_friend_uri',
 
         column_defs => {
-            'id'            => 'integer not null auto_increment',
-            'friend_id'     => 'integer not null',
-            'author_id'	    => 'integer not null',
-            'uri'           => 'text not null',
-            'source_uri'    => 'text',
-            'description'   => 'text',
-            'target'        => 'string(255)',
-            'feed_uri'      => 'text',
-            'notes'         => 'text',
-            'rating'        => 'integer',
-            'is_subscribed' => 'integer',
-            'last_updated'  => 'timestamp',
+            'id'           => 'integer not null auto_increment',
+            'friend_id'    => 'integer not null',
+            'author_id'    => 'integer not null',
+            'uri'          => 'text not null',
+            'label'        => 'text',
+            'notes'        => 'text',
+            'last_updated' => 'timestamp',
         },
         class_type  => 'uri',
         primary_key => 'id',
@@ -29,12 +24,12 @@ __PACKAGE__->install_properties(
 );
 
 sub friend {
-    my $uri = shift;
+    my $uri          = shift;
     my $friend_class = MT->model('friend');
-    $friend = Friends::Friend->load( $uri->friend_id );
-	return $friend;
+    my $friend = $friend_class->load( { id => $uri->friend_id } );
+    return $friend;
 }
 
-sub class_label { MT->translate('URL'); }
+sub class_label        { MT->translate('URL'); }
 sub class_label_plural { MT->translate('URLs'); }
 1;
