18eaf484837245b0a00136498b8d8e07f0ed7a56
Byrne Reese <byrne@majordojo.com>
2008-06-04 07:48:50 +0000

    Made a number of changes to improve OpenID support and to create better interoperability with Yahoo's OpenID 2.0 implementation:
    * X-XRDS-Location header is now returned when making any request to the 
      MT::App::Comments application or mt-comments.cgi script
    * Added support for a new service registry key: http_method.
      When NOT specified this is how a URI will be published:
        <URI>http://www.majordojo.com/cgi-bin/mt/mt-comments.cgi</URI>
      When specified this is how:
        <URI simple:http_method="GET">http://www.majordojo.com/cgi-bin/mt/mt-comments.cgi</URI>

--- movabletype/XRDS_Simple/trunk/plugins/XRDS_Simple/lib/XRDS_Simple/App/Plugin.pm
+++ movabletype/XRDS_Simple/trunk/plugins/XRDS_Simple/lib/XRDS_Simple/App/Plugin.pm
@@ -7,47 +7,59 @@ sub init_request {
     my $app = shift;
     $app->SUPER::init_request(@_);
     $app->{requires_login} = 0;
-	$app->{is_admin} = 0;
-	$app;
+    $app->{is_admin} = 0;
+    $app;
+}
+
+sub init_app { 
+    my $plugin = shift;
+    my ($app) = @_;
+# I am not sure I need to do this
+#    MT->add_callback('MT::App::CMS::init_request', 1, $app, sub { 
+#	$app->set_header( 'X-XRDS-Location' => $app->base . $app->mt_uri('mode' => 'xrds_simple') ); 
+#    });
+    MT->add_callback('MT::App::Comments::init_request', 1, $app, sub { 
+	$app->set_header( 'X-XRDS-Location' => $app->base . $app->mt_uri('mode' => 'xrds_simple') ); 
+    });
+    1;
 }
 
 sub xrds_simple {
-	my $app = shift;
-	$app->{requires_login} = 0;
-	#my $logger = MT::Log->get_logger();
-	
-	# load registry xrds_services
-	my $xrds_services = $app->registry('xrds_services') || {};
-	
-	my @services;
-	
-	foreach my $service_name (keys %{$xrds_services}) {
-		my $service_def = $xrds_services->{$service_name};
-		# TODO: support coderefs for each parameter (expires, uri, mediatype, etc.)
-		if ($service_def->{local_id_handler}) {
-			my $local_id_handler = $app->handler_to_coderef($service_def->{local_id_handler})
-			        or return $app->error('local_id_handler call failed: ' . $!);
-			    
-			$service_def->{local_id} = $local_id_handler->();
-		}
-		if ($service_def->{registered_services}) {
-			my @rservices;
-			foreach my $rservice (keys %{$service_def->{registered_services}}) {
-				
-				push @rservices, $service_def->{registered_services}->{$rservice};
-			}
-			$service_def->{registered_services} = \@rservices;
-		}
-		MT->log(Dumper($service_def));
-		push @services, $service_def;
+    my $app = shift;
+    $app->{requires_login} = 0;
+    #my $logger = MT::Log->get_logger();
+    
+    # load registry xrds_services
+    my $xrds_services = $app->registry('xrds_services') || {};
+    
+    my @services;
+    
+    foreach my $service_name (keys %{$xrds_services}) {
+	my $service_def = $xrds_services->{$service_name};
+	# TODO: support coderefs for each parameter (expires, uri, mediatype, etc.)
+	if ($service_def->{local_id_handler}) {
+	    my $local_id_handler = $app->handler_to_coderef($service_def->{local_id_handler})
+		or return $app->error('local_id_handler call failed: ' . $!);
+	    $service_def->{local_id} = $local_id_handler->();
 	}
-	
+	if ($service_def->{registered_services}) {
+	    my @rservices;
+	    foreach my $rservice (keys %{$service_def->{registered_services}}) {
+		push @rservices, $service_def->{registered_services}->{$rservice};
+	    }
+	    $service_def->{registered_services} = \@rservices;
+	}
+#	MT->log(Dumper($service_def));
+	push @services, $service_def;
+    }
+    
     my $plugin = $app->component('xrds_simple');
     
-	# now serve the file
-	# $logger->debug($app->request_method);
+    # now serve the file
+    # $logger->debug($app->request_method);
     if ( 'GET' eq $app->request_method ) {
-        $app->response_content_type('text/plain');
+        $app->response_content_type('application/xrds+xml');
+#        $app->response_content_type('text/plain');
         if ( 'xrds_simple' eq $app->mode ) {
             my $param = {
                 services => \@services,
@@ -56,15 +68,15 @@ sub xrds_simple {
         }
     }
     elsif ( 'HEAD' eq $app->request_method ) {
-		#$app->set_header( 'X-XRDS-Location' => $app->base . $app->mt_uri('mode' => $app->mode) );
-		$app->response_content_type('application/xrds+xml');
-		return q();
+	#$app->set_header( 'X-XRDS-Location' => $app->base . $app->mt_uri('mode' => $app->mode) );
+	$app->response_content_type('application/xrds+xml');
+	return q();
     }
     return $app->error($plugin->translate('Invalid request'));
 }
 
 sub openid_local_id {
-	"steve";
+    "steve";
 }
 
 1;
