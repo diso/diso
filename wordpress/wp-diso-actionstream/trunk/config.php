d19581d3c157b851938d01600140b90d7916539e
wnorris <wnorris@1fe52bb4-7940-0410-8bb1-4d0b51536974>
2008-07-04 19:33:31 +0000

    use wordpress hook instead of register_actionstream_service method.  optimize database call when polling (also eliminates need for 'actionstream' array in wp_options table)

--- wordpress/wp-diso-actionstream/trunk/config.php
+++ wordpress/wp-diso-actionstream/trunk/config.php
@@ -9,8 +9,12 @@ function get_actionstream_config() {
 	if(!$yaml) {
 		$yaml = Spyc::YAMLLoad(dirname(__FILE__).'/config.yaml');//file straight from MT plugin - yay sharing!
 		$streams = get_option('actionstream_streams');
-		$yaml['action_streams'] = array_merge($yaml['action_streams'], $streams ? $streams : array());
 		$services = get_option('actionstream_services');
+		
+		// do filter
+		list($services, $streams) = apply_filters('actionstream_services', $services, $streams);
+
+		$yaml['action_streams'] = array_merge($yaml['action_streams'], $streams ? $streams : array());
 		$yaml['profile_services'] = array_merge($yaml['profile_services'], $services ? $services : array());
 	}
 	return $yaml;
