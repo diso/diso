b0d972cd88e87436140da61fa4f5d251eeb237e8
Will Norris <will@willnorris.com>
2008-08-04 23:02:16 +0000

    pass single object through actionstream_services filter

--- wordpress/wp-diso-actionstream/trunk/config.php
+++ wordpress/wp-diso-actionstream/trunk/config.php
@@ -8,14 +8,14 @@ function get_actionstream_config() {
 	static $yaml;
 	if(!$yaml) {
 		$yaml = Spyc::YAMLLoad(dirname(__FILE__).'/config.yaml');//file straight from MT plugin - yay sharing!
-		$streams = get_option('actionstream_streams');
-		$services = get_option('actionstream_services');
-		
-		// do filter
-		list($services, $streams) = apply_filters('actionstream_services', $services, $streams);
 
-		$yaml['action_streams'] = array_merge($yaml['action_streams'], $streams ? $streams : array());
-		$yaml['profile_services'] = array_merge($yaml['profile_services'], $services ? $services : array());
+		$actionstream['streams'] = get_option('actionstream_streams');
+		$actionstream['services'] = get_option('actionstream_services');
+		
+		$add_services = apply_filters('actionstream_services', array('streams' => array(), 'services' => array()) );
+
+		$yaml['action_streams'] = array_merge($yaml['action_streams'], $add_services['streams']);
+		$yaml['profile_services'] = array_merge($yaml['profile_services'], $add_services['services']);
 	}
 	return $yaml;
 }//end function get_actionstream_config
