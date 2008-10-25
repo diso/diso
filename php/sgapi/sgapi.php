c089f26a02ffa1b81b1ab4337cde2cfa7914c35b
Will Norris <will@willnorris.com>
2008-10-25 01:06:45 +0000

    cleanup SGAPI class and add two convenience methods

--- php/sgapi/sgapi.php
+++ php/sgapi/sgapi.php
@@ -1,25 +1,54 @@
 <?php
 
-require 'JSON.php';
-define ('APIURL','http://socialgraph.apis.google.com/lookup');
-$JSON = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
 
+/**
+ * Socaial Graph API.
+ *
+ * @see http://code.google.com/apis/socialgraph/docs/api.html
+ */
 class SocialGraphApi {
+
+	/** API URL */
+	var $api_url = 'http://socialgraph.apis.google.com/lookup';
+
+	/** JSON parser */
+	var $json;
+
+	/** boolean - Return edges out from returned nodes. */
+	var $edges_out;
+
+	/** boolean - Return edges in to returned nodes. */
+	var $edges_in;
+
+	/** boolean - Follow me links, also returning reachable nodes. */
+	var $follow_me;
+
+	/** boolean - Return internal representation of nodes. */
+	var $sgn;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Array optional parameters.
+	 */
 	function SocialGraphApi($params) {
-		
-		// edo 	boolean 	Return edges out from returned nodes. -> edgesout
-		// edi 	boolean 	Return edges in to returned nodes. -> edgesin
-		// fme 	boolean 	Follow me links, also returning reachable nodes. -> followme
-		// sgn 	boolean 	Return internal representation of nodes.
-		
-		$this->edgesout = isset($params['edgesout']) ? $params['edgesout'] : '0';
-		$this->edgesin = isset($params['edgesin']) ? $params['edgesin'] : '0';
-		$this->followme = isset($params['followme']) ? $params['followme'] : '0';
+		require_once 'JSON.php';
+		$this->json = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+		$this->edges_out = isset($params['edgesout']) ? $params['edgesout'] : '0';
+		$this->edges_in = isset($params['edgesin']) ? $params['edgesin'] : '0';
+		$this->follow_me = isset($params['followme']) ? $params['followme'] : '0';
 		$this->sgn = isset($params['sgn']) ? $params['sgn'] : '0';
 	}
 	
+
+	/**
+	 * Get the Social Graph data for the specified URIs.
+	 *
+	 * @param mixed $uris URIs to lookup
+	 * @return Array social graph data
+	 */
 	function get($uris) {
-		global $JSON;
 		// is array? implode else pass
 		if (is_array($uris)) {
 			$uris = implode(',',$uris);
@@ -28,12 +57,12 @@ class SocialGraphApi {
 		
 		$qs = '';
 		$qs .= 'q=' . $uris . 
-			   '&edo=' . $this->edgesout .
-			   '&edi=' . $this->edgesin .
-			   '&fme=' . $this->followme .
+			   '&edo=' . $this->edges_out .
+			   '&edi=' . $this->edges_in .
+			   '&fme=' . $this->follow_me .
 			   '&sgn=' . $this->sgn;
 		
-		$ch = curl_init(APIURL."?$qs");
+		$ch = curl_init($this->api_url."?$qs");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$result = curl_exec($ch);
 		curl_close($ch);
@@ -41,7 +70,60 @@ class SocialGraphApi {
 		if (empty($result)) return null;
 		
 		//TODO: handle errors
-		$this->data = $JSON->decode($result);
+		$this->data = $this->json->decode($result);
 		return $this->data;
 	}
+
+
+	/**
+	 * Get all the nodes claimed by the specified URIs.
+	 *
+	 * @param mixed $uris URIs to lookup
+	 * @return Array claimed nodes
+	 */
+	public static function get_claimed($uris) {
+		$sgapi = new SocialGraphApi(array('followme'=>1));
+		$data = $sgapi->get($uris);
+		$canonical = array_unique(array_values($data['canonical_mapping']));
+
+		$claimed = array();
+		foreach ($canonical as $c) {
+			$claimed = array_merge($claimed, $data['nodes'][$c]['claimed_nodes']);
+		}
+
+		return array_unique(array_merge($canonical, $claimed));
+	}
+
+
+	/**
+	 * Get all the verified equivalent nodes.  These are nodes that have a 
+	 * bi-directional "me" relationship with one of the specified URIs.
+	 *
+	 * @param mixed $uris URIs to lookup
+	 * @return Array equivalent nodes
+	 */
+	public static function get_equivalent($uris) {
+		$sgapi = new SocialGraphApi(array('followme'=>1));
+		$data = $sgapi->get($uris);
+		$canonical = array_unique(array_values($data['canonical_mapping']));
+
+		$claimed = array();
+		foreach ($canonical as $c) {
+			$claimed = array_merge($claimed, 
+				$data['nodes'][$c]['claimed_nodes']);
+		}
+
+		$verified = array();
+		foreach ($claimed as $c) {
+			foreach ($data['nodes'][$c]['claimed_nodes'] as $node) {
+				if (in_array($node, $canonical)) {
+					$verified[] = $c;
+					continue 2;
+				}
+			}
+		}
+
+		return array_unique(array_merge($canonical, $verified));
+	}
+
 }
