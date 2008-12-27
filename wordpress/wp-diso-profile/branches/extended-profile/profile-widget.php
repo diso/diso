622ceb15f7557f7881f8c54aea5f68c66c78db48
wnorris <wnorris@1fe52bb4-7940-0410-8bb1-4d0b51536974>
2008-12-27 09:03:20 +0000

    lots of code formatting, and remove direct wpdb calls (also removes default user... perhaps try and bring this back in?)

--- wordpress/wp-diso-profile/branches/extended-profile/profile-widget.php
+++ wordpress/wp-diso-profile/branches/extended-profile/profile-widget.php
@@ -1,5 +1,6 @@
 <?php
 
+// TODO migrate existing diso-profile widget
 function widget_ext_profile_init() {
 
 	if (!function_exists('register_sidebar_widget'))
@@ -14,40 +15,42 @@ function widget_ext_profile_init() {
 
 		echo $before_widget;
 		echo $before_title . $title . $after_title;
-		extended_profile($options['userid']);
+		extended_profile($options['user']);
 		echo $after_widget;
 	}
 	
 	function widget_ext_profile_control() {
-		global $wpdb;
-
 		$options = get_option('widget_ext_profile');
+
 		if ( !is_array($options) )
-			$options = array('title'=>'Extended Profile', 'userid'=>false);
-		if ( $_POST['submit'] ) {
-			$options['title'] = strip_tags(stripslashes($_POST['title']));
-			$options['userid'] = strip_tags(stripslashes($_POST['userid']));
+			$options = array('title'=>'User Profile', 'user'=>false);
+		
+		if ( $_POST['profile_submit'] ) {
+			$options['title'] = strip_tags(stripslashes($_POST['profile_title']));
+			$options['user'] = strip_tags(stripslashes($_POST['profile_user']));
 			update_option('widget_ext_profile', $options);
 		}
 
 		$title = htmlspecialchars($options['title'], ENT_QUOTES);
+		?>
 
-		echo '<p style="text-align:right;"><label for="title">Title:</label><br /> <input style="width: 200px;" id="title" name="title" type="text" value="'.$title.'" /></p>';
+		<p style="text-align:right;">
+			<label for="profile_title">Title:</label><br /> 
+			<input style="width: 200px;" id="profile_title" name="profile_title" type="text" value="'.$title.'" />
+		</p>
 
-		echo '<p style="text-align:right;"><label for="userid">User:</label><br /> ';
-		echo '	<select style="width: 200px;" id="userid" name="userid">';
-		$users = $wpdb->get_results("SELECT display_name,ID FROM $wpdb->users ORDER BY user_registered,ID");
-		foreach($users as $user)
-			echo '		<option value="'.$user->ID.'"'.($options['userid'] == $user->ID ? ' selected="selected"' : '').'>'.htmlspecialchars($user->display_name).'</option>';
-		echo '	</select>';
-		echo '</p>';
+		<p style="text-align:right;"><label for="profile_user">User:</label><br /> 
+			<?php wp_dropdown_users(array('selected' => $options['user'], 'name' => 'profile_user')); ?>
+		</p>
+		<style type="text/css"> #profile_user { width: 200px; } </style>
 
-		echo '<input type="hidden" id="submit" name="submit" value="1" />';
+		<input type="hidden" id="profile_submit" name="profile_submit" value="1" />
+
+		<?php
 	}
 	
-			
-	register_sidebar_widget('Extended Profile', 'widget_ext_profile');
-	register_widget_control('Extended Profile', 'widget_ext_profile_control', 270, 270);
+	register_sidebar_widget('User Profile', 'widget_ext_profile');
+	register_widget_control('User Profile', 'widget_ext_profile_control');
 }
 
 add_action('plugins_loaded', 'widget_ext_profile_init');
