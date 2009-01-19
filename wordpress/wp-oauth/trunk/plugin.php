e3325eda032066b579a7aeb507a2a97d7e9ee59e
wnorris <wnorris@1fe52bb4-7940-0410-8bb1-4d0b51536974>
2009-01-19 22:02:29 +0000

    merge changes from refactoring branch to trunk

--- wordpress/wp-oauth/trunk/plugin.php
+++ wordpress/wp-oauth/trunk/plugin.php
@@ -1,103 +1,76 @@
 <?php
-/*
-Plugin Name: WP-OAuth
-Plugin URI: http://singpolyma.net/plugins/oauth/
-Description: Enables OAuth services on your Wordpress blog.
-Version: 0.13
-Author: Stephen Paul Weber
-Author URI: http://singpolyma.net/
-*/
 
-//Licensed under an MIT-style licence
+/**
+ * Register XRDS Services.
+ */
+function oauth_xrds_service($xrds) {
 
-function oauth_accept() {
+	$xrds = xrds_add_service($xrds, 'main', 'OAuth Dummy Service', array(
+		'Type' => array( array('content' => 'http://oauth.net/discovery/1.0') ),
+		'URI' => array( array('content' => '#oauth' ) ),
+	) );
 
-	require_once dirname(__FILE__).'/common.inc.php';
-	require_once dirname(__FILE__).'/../../../wp-includes/pluggable.php';
-	@include_once dirname(__FILE__).'/../xrds-simple.php';
+	$xrds = xrds_add_service($xrds, 'oauth', 'OAuth Request Token', array(
+		'Type' => array( 
+			array('content' => 'http://oauth.net/core/1.0/endpoint/request'),
+			array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
+			array('content' => 'http://oauth.net/core/1.0/signature/HMAC-SHA1'),
+		),
+		'URI' => array( array('content' => get_option('siteurl').'/?_oauth_endpoint=request' ) ),
+	) );
+
+	$xrds = xrds_add_service($xrds, 'oauth', 'OAuth Authorize Token', array(
+		'Type' => array( 
+			array('content' => 'http://oauth.net/core/1.0/endpoint/authorize'),
+			array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
+		),
+		'URI' => array( array('content' => get_option('siteurl').'/?_oauth_endpoint=authorize' ) ),
+	) );
 	
-	if(function_exists('register_xrd')) {
-		$xrds = get_option('xrds_simple');
-		if(!$xrds['oauth']) {
-			register_xrd_service('main', 'OAuth Dummy Service', array(
-				'Type' => array( array('content' => 'http://oauth.net/discovery/1.0') ),
-				'URI' => array( array('content' => '#oauth' ) ),
-			) );
+	$xrds = xrds_add_service($xrds, 'oauth', 'OAuth Access Token', array(
+		'Type' => array( 
+			array('content' => 'http://oauth.net/core/1.0/endpoint/access'),
+			array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
+			array('content' => 'http://oauth.net/core/1.0/signature/HMAC-SHA1'),
+		),
+		'URI' => array( array('content' => get_option('siteurl').'/?_oauth_endpoint=access' ) ),
+	) );
+	
+	$xrds = xrds_add_service($xrds, 'oauth', 'OAuth Resources', array(
+		'Type' => array( 
+			array('content' => 'http://oauth.net/core/1.0/endpoint/resource'),
+			array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
+			array('content' => 'http://oauth.net/core/1.0/signature/HMAC-SHA1'),
+		),
+	) );
+	
+	$xrds = xrds_add_service($xrds, 'oauth', 'OAuth Static Token', array(
+		'Type' => array( 
+			array('content' => 'http://oauth.net/discovery/1.0/consumer-identity/static'),
+		),
+		'LocalID' => array( array('content' => 'DUMMYKEY' ) ),
+	) );
 
-			register_xrd('oauth');
-			register_xrd_service('oauth', 'OAuth Request Token', array(
-				'Type' => array( 
-					array('content' => 'http://oauth.net/core/1.0/endpoint/request'),
-					array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
-					array('content' => 'http://oauth.net/core/1.0/signature/HMAC-SHA1'),
-				),
-				'URI' => array( array('content' => get_bloginfo('wpurl').'/wp-content/plugins/wp-oauth/request_token.php' ) ),
-			) );
-			register_xrd_service('oauth', 'OAuth Authorize Token', array(
-				'Type' => array( 
-					array('content' => 'http://oauth.net/core/1.0/endpoint/authorize'),
-					array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
-				),
-				'URI' => array( array('content' => get_bloginfo('wpurl').'/wp-content/plugins/wp-oauth/authorize_token.php' ) ),
-			) );
-			register_xrd_service('oauth', 'OAuth Access Token', array(
-				'Type' => array( 
-					array('content' => 'http://oauth.net/core/1.0/endpoint/access'),
-					array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
-					array('content' => 'http://oauth.net/core/1.0/signature/HMAC-SHA1'),
-				),
-				'URI' => array( array('content' => get_bloginfo('wpurl').'/wp-content/plugins/wp-oauth/access_token.php' ) ),
-			) );
-			register_xrd_service('oauth', 'OAuth Resources', array(
-				'Type' => array( 
-					array('content' => 'http://oauth.net/core/1.0/endpoint/resource'),
-					array('content' => 'http://oauth.net/core/1.0/parameters/uri-query'),
-					array('content' => 'http://oauth.net/core/1.0/signature/HMAC-SHA1'),
-				),
-			) );
-			register_xrd_service('oauth', 'OAuth Static Token', array(
-				'Type' => array( 
-					array('content' => 'http://oauth.net/discovery/1.0/consumer-identity/static'),
-				),
-				'LocalID' => array( array('content' => 'DUMMYKEY' ) ),
-			) );
-		}//end if ! oauth
-	}//end if register_xrd
+	return $xrds;
+}
+add_filter('xrds_simple', 'oauth_xrds_service');
 
-	$services = get_option('oauth_services');
+
+/**
+ * Register OAuth basic services
+ */
+function oauth_basic_services($services) {
 	$services['Post Comments'] = array('wp-comments-post.php');
 	$services['Edit and Create Entries and Categories'] = array('wp-app.php');
-	update_option('oauth_services', $services);
 
-	$store = new OAuthWordpressStore();
-	$server = new OAuthServer($store);
-	$sha1_method = new OAuthSignatureMethod_HMAC_SHA1();
-	$plaintext_method = new OAuthSignatureMethod_PLAINTEXT();
-	$server->add_signature_method($sha1_method);
-	$server->add_signature_method($plaintext_method);
+	return $services;
+}
+add_filter('oauth_services', 'oauth_basic_services');
 
-	try {
-		$req = OAuthRequest::from_request();
-		list($consumer, $token) = $server->verify_request($req);
-		$userid = $store->user_from_token($consumer->key, $token->key);
-		$authed = get_usermeta($userid, 'oauth_consumers');
-		$authed = $authed[$consumer->key];
-		if($authed && $authed['authorized']) {
-			$allowed = false;
-			foreach($authed as $ends)
-				if(is_array($ends))
-					foreach($ends as $end)
-						if(strstr($_SERVER['SCRIPT_URI'], $end))
-							$allowed = true;
-			if($allowed)
-				set_current_user($userid);
-		}//end if
-	} catch (OAuthException $e) {/* We may not be doing OAuth at all.  */}
 
-}//end function oauth_accept
-//if(!$NO_oauth)
-	oauth_accept();
-
+/**
+ * OAuth configuration page
+ */
 function oauth_page() {
 	global $wpdb;
 	if($_POST['new_consumer']) {
@@ -129,11 +102,11 @@ function oauth_page() {
 	echo '<h3>Endpoints</h3>';
 	echo '<dl>';
 	echo '	<dt>Request token endpoint</dt>';
-	echo '		<dd>'.get_bloginfo('wpurl').'/wp-content/plugins/wp-oauth/request_token.php</dd>';
+	echo '		<dd>'.get_option('siteurl').'/?_oauth_endpoint=request</dd>';
 	echo '	<dt>Authorize token endpoint</dt>';
-	echo '		<dd>'.get_bloginfo('wpurl').'/wp-content/plugins/wp-oauth/authorize_token.php</dd>';
+	echo '		<dd>'.get_option('siteurl').'/?_oauth_endpoint=authorize</dd>';
 	echo '	<dt>Access token endpoint</dt>';
-	echo '		<dd>'.get_bloginfo('wpurl').'/wp-content/plugins/wp-oauth/access_token.php</dd>';
+	echo '		<dd>'.get_option('siteurl').'/?_oauth_endpoint=access</dd>';
 	echo '</dl>';
 	$anid = $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_type='post' ORDER BY post_date DESC LIMIT 1");
 	echo '<a href="http://singpolyma.net/oauth/example/wp_auto_client3.php?'
@@ -143,12 +116,20 @@ function oauth_page() {
 .'">Click here for a test page &raquo;</a>';
 }//end function oauth_page
 
+
+/**
+ * Add OAuth configuration page.
+ */
 function oauth_tab($s) {
-	add_submenu_page('options-general.php', 'OAuth', 'OAuth', 1, __FILE__, 'oauth_page');
+	add_submenu_page('options-general.php', 'OAuth', 'OAuth', 1, 'wp-oauth', 'oauth_page');
 	return $s;
-}//end function
+}
 add_action('admin_menu', 'oauth_tab');
 
+
+/**
+ * Output all OAuth services.
+ */
 function oauth_services_render() {
 	global $userdata;
 	get_currentuserinfo();
@@ -181,11 +162,222 @@ function oauth_services_render() {
 	echo '		<p><input type="submit" name="save" value="Save &raquo;" /></p>';
 	echo '	</form>';
 	echo '</div>';
-}//end function oauth_services_render
+}
+
+
+/**
+ * Add OAuth profile configuration page
+ */
 function oauth_services_tab($s) {
 	add_submenu_page('profile.php', 'Services', 'Services', 1, __FILE__, 'oauth_services_render');
 	return $s;
-}//end function
+}
 add_action('admin_menu', 'oauth_services_tab');
+
+
+/**
+ * Parse the incoming WordPress request.  If it is an OAuth request, handle it accordingly.
+ */
+function oauth_parse_request($wp) {
+	if (array_key_exists('_oauth_endpoint', $_REQUEST) && function_exists('oauth_endpoint_' . $_REQUEST['_oauth_endpoint'])) {
+		call_user_func('oauth_endpoint_' . $_REQUEST['_oauth_endpoint']);
+	}
+
+	// if we have an oauth_token but no _oauth_endpoint, this must be an oauth request for a protected resource
+	if (array_key_exists('oauth_token', $_REQUEST)) {
+		oauth_protected_resource();
+	}
+}
+add_action('parse_request', 'oauth_parse_request');
+
+
+/**
+ * Process an OAuth request for a protected resource.
+ */
+function oauth_protected_resource() {
+	require_once dirname(__FILE__).'/common.inc.php';
+
+	$services = apply_filters('oauth_services', array());
+
+	$store = new OAuthWordpressStore();
+	$server = oauth_init_server($store);
+
+	try {
+		$req = OAuthRequest::from_request();
+		list($consumer, $token) = $server->verify_request($req);
+		$userid = $store->user_from_token($consumer->key, $token->key);
+		$authed = get_usermeta($userid, 'oauth_consumers');
+		$authed = $authed[$consumer->key];
+		if($authed && $authed['authorized']) {
+			$allowed = false;
+			foreach($authed as $ends)
+				if(is_array($ends))
+					foreach($ends as $end)
+						if(strstr($_SERVER['SCRIPT_URI'], $end))
+							$allowed = true;
+			if($allowed)
+				set_current_user($userid);
+		}//end if
+	} catch (OAuthException $e) {/* We may not be doing OAuth at all.  */}
+}
+
+
+/**
+ * Initialize an OAuth Server instance.
+ */
+function oauth_init_server($store = null) {
+	require_once dirname(__FILE__).'/common.inc.php';
+
+	if (!$store) $store = new OAuthWordpressStore();
+	$server = new OAuthServer($store);
+	$sha1_method = new OAuthSignatureMethod_HMAC_SHA1();
+	$plaintext_method = new OAuthSignatureMethod_PLAINTEXT();
+	$server->add_signature_method($sha1_method);
+	$server->add_signature_method($plaintext_method);
+
+	return $server;
+}
+
+
+/**
+ * Process request for an OAuth request token.
+ */
+function oauth_endpoint_request() {
+	require_once dirname(__FILE__).'/common.inc.php';
+	$store = new OAuthWordpressStore();
+	$server = oauth_init_server($store);
+
+	try {
+	  $req = OAuthRequest::from_request();
+	  $token = $server->fetch_request_token($req);
+	  print $token.'&xoauth_token_expires='.urlencode($store->token_expires($token));
+	  exit;
+	} catch (OAuthException $e) {
+	  header('Content-type: text/plain;', true, 400);
+	  print($e->getMessage() . "\n\n");
+	  var_dump($req);
+	  die;
+	}
+}
+
+
+/**
+ * Proces request for OAuth token authorization.
+ */
+function oauth_endpoint_authorize() {
+	global $wpdb;
+
+	if(!$_REQUEST['oauth_token'] && !$_POST['authorize']) die('No token passed');
+
+	$NO_oauth = true;
+	require_once dirname(__FILE__).'/common.inc.php';
+	$store = new OAuthWordpressStore();
+
+	if(!$_POST['authorize']) {
+		$token = $wpdb->escape($_REQUEST['oauth_token']);
+		$consumer_key = $store->lookup_token('','request',$token);//verify token
+		if(!$consumer_key) die('Invalid token passed');
+	}//end if ! POST authorize
+
+	$user = wp_get_current_user();
+
+	if(!$user->ID) {
+		$redirect_to = urlencode(get_option('siteurl').'/?_oauth_endpoint=authorize&oauth_token='.urlencode($_REQUEST['oauth_token']).'&oauth_callback='.urlencode($_REQUEST['oauth_callback']));
+		header('Location: '.get_option('siteurl').'/wp-login.php?redirect_to='.$redirect_to,true,303);
+		exit;
+	}//end if ! userdata->ID
+
+	if($_POST['authorize']) {
+		session_start();
+		$_REQUEST['oauth_callback'] = $_SESSION['oauth_callback']; unset($_SESSION['oauth_callback']);
+		$token = $_SESSION['oauth_token']; unset($_SESSION['oauth_token']);
+		$consumer_key = $_SESSION['oauth_consumer_key']; unset($_SESSION['oauth_consumer_key']);
+		if($_POST['authorize'] != 'Ok') {
+			if($_REQUEST['oauth_callback']) {
+				header('Location: '.$_REQUEST['oauth_callback'],true,303);
+			} else {
+				get_header();
+				echo '<h2 style="text-align:center;">You chose to cancel authorization.  You may now close this window.</h2>';
+				get_footer();
+			}//end if-else callback
+			exit;
+		}//cancel authorize
+		$consumers = get_usermeta($user->ID, 'oauth_consumers');
+		if (!$consumers) $consumers = array();
+		$services = apply_filters('oauth_services', array());
+		$yeservices = array();
+		foreach($services as $k => $v)
+			if(in_array($k, array_keys($_REQUEST['services'])))
+				$yeservices[$k] = $v;
+		$consumers[$consumer_key] = array_merge(array('authorized' => true), $yeservices);//it's an array so that more granular data about permissions could go in here
+		update_usermeta($user->ID, 'oauth_consumers', $consumers);
+	}//end if authorize
+
+	$oauth_consumers = get_usermeta($user->ID, 'oauth_consumers');
+	if($oauth_consumers && in_array($consumer_key,array_keys($oauth_consumers))) {
+		$store->authorize_request_token($consumer_key, $token, $user->ID);
+		if($_REQUEST['oauth_callback']) {
+			header('Location: '.$_REQUEST['oauth_callback'],true,303);
+		} else {
+			get_header();
+			echo '<h2 style="text-align:center;">Authorized!  You may now close this window.</h2>';
+			get_footer();
+		}//end if-else callback
+		exit;
+	} else {
+		session_start();//use a session to prevent the consumer from tricking the user into posting the Yes answer
+		$_SESSION['oauth_token'] = $token;
+		$_SESSION['oauth_callback'] = $_REQUEST['oauth_callback'];
+		$_SESSION['oauth_consumer_key'] = $consumer_key;
+		get_header();
+		$description = $store->lookup_consumer_description($consumer_key);
+		if($description) $description = 'Allow '.$description.' to access your Wordpress account and...';
+			else $description = 'Allow the service you came from to access your Wordpress account and...';
+		?>
+		<div style="text-align:center;">
+			<h2><?php echo $description; ?></h2>
+			<form method="post" action="?_oauth_endpoint=authorize"><div>
+				<div style="text-align:left;width:15em;margin:0 auto;">
+					<ul style="padding:0px;">
+				<?php
+					$services = apply_filters('oauth_services', array());
+					foreach($services as $k => $v)
+						echo '<li><input type="checkbox" checked="checked" name="services['.htmlentities($k).']" /> '.$k.'</li>';
+				?>
+					</ul>
+					<br />
+					<input type="submit" name="authorize" value="Ok" />
+					<input type="submit" name="authorize" value="No" />
+				</div>
+			</div></form>
+		</div>
+		<?php
+		get_footer();
+	}//end if user has authorized this consumer
+
+	exit;
+}
+
+
+/**
+ * Process request for an OAuth access token.
+ */
+function oauth_endpoint_access() {
+	require_once dirname(__FILE__).'/common.inc.php';
+	$store = new OAuthWordpressStore();
+	$server = oauth_init_server($store);
+
+	try {
+	  $req = OAuthRequest::from_request();
+	  $token = $server->fetch_access_token($req);
+	  print $token.'&xoauth_token_expires='.urlencode($store->token_expires($token));
+	  exit;
+	} catch (OAuthException $e) {
+	  header('Content-type: text/plain;', true, 400);
+	  print($e->getMessage() . "\n\n");
+	  var_dump($req);
+	  die;
+	}
+}
 
 ?>
