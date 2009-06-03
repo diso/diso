6fff886b949aedf45f0faefd222afd8692585846
wnorris <wnorris@1fe52bb4-7940-0410-8bb1-4d0b51536974>
2009-06-03 23:48:30 +0000

    no point in abstracting wpdb since so much else is wp specific

--- wordpress/wp-diso-actionstream/trunk/config.php
+++ wordpress/wp-diso-actionstream/trunk/config.php
@@ -1,29 +1,35 @@
 <?php
 
-global $wpdb;
-
+/**
+ * Load YAML file of Activity Stream services.
+ *
+ * @return array configuration array with two keys: action_streams and profile_services
+ */
 function get_actionstream_config() {
-	if(!class_exists('Spcy'))
-		require_once dirname(__FILE__).'/lib/spyc.php';
 	static $yaml;
-	if(!$yaml) {
+
+	if ( !$yaml ) {
+		if ( !class_exists('Spcy') ) {
+			require_once dirname(__FILE__).'/lib/spyc.php';
+		}
+
 		$yaml = Spyc::YAMLLoad(dirname(__FILE__).'/config.yaml');//file straight from MT plugin - yay sharing!
 
-		$actionstream['streams'] = get_option('actionstream_streams');
-		$actionstream['services'] = get_option('actionstream_services');
-		
 		$add_services = apply_filters('actionstream_services', array('streams' => array(), 'services' => array()) );
 
 		$yaml['action_streams'] = array_merge($yaml['action_streams'], $add_services['streams']);
 		$yaml['profile_services'] = array_merge($yaml['profile_services'], $add_services['services']);
 	}
-	return $yaml;
-}//end function get_actionstream_config
 
-global $actionstream_config;
-$actionstream_config = array(
-		'db' => $wpdb,
-		'item_table' => $wpdb->prefix.'actionstream_items'
-	);
+	return $yaml;
+}
+
+/**
+ * Get the name of the items SQL table.
+ */
+function activity_stream_items_table() {
+	global $wpdb;
+	return $wpdb->prefix . 'actionstream_items';
+}
 
 ?>
